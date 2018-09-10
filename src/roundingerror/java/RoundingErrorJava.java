2ece918e1ac404e56b1fee08af016e00606f1efb
Runeslayer10 <43037188+Runeslayer10@users.noreply.github.com>
2018-09-10 14:21:53 -0400

    Update RoundingErrorJava.java

--- src/roundingerror/java/RoundingErrorJava.java
+++ src/roundingerror/java/RoundingErrorJava.java
@@ -5,7 +5,7 @@
  */
 
 package roundingerror.java;
-import javax.swing.*;
+import javax.swing.*; // allows me to use JOptionPane
 /**
  *
  * @author blhad3491
@@ -18,11 +18,12 @@ public class RoundingErrorJava {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-String titlestring = JOptionPane.showInputDialog("Please Insert Number To Be Squared");
-    double number = Double.parseDouble(titlestring);
-    double SqRtNum = Math.pow(Math.sqrt(number),2);
-    JOptionPane.showMessageDialog(null,"The number you squared and squarerooted was"+" " +number + 
-            "\nThe product of that equation is"+ " "+SqRtNum);
+String titlestring = JOptionPane.showInputDialog("Please Insert Number To Be Squared"); // string variable for the user to have somewhere to input there data
+    double number = Double.parseDouble(titlestring); // this is making it so that the user can insert numbers.
+    double SqRtNum = Math.pow(Math.sqrt(number),2); // The user inputs a number to be squarerooted and then squared
+    JOptionPane.showMessageDialog(null,"The number you squaredrooted and then squared was"+" " +number + 
+            "\nThe product of that equation is"+ " "+SqRtNum); // Displaying the text after user inserts data
+        
     
     }
     
